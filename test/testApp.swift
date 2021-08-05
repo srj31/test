6ab7d0afef13aa26cdf346d3ea964a2f311ac3b7
Sourabh Jaiswal <sourabhrj31@gmail.com>
2021-08-05 22:06:17 +0800

    initial commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Sourabh Jaiswal on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
